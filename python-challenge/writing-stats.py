c782c4eb5bbd7ba8d71bfc1cb6093f5336ac08fb
James <james@james.com>
2016-02-01 21:16:05 +0000

    Now with improved error handling, just need to pretty things up

--- python-challenge/writing-stats.py
+++ python-challenge/writing-stats.py
@@ -28,20 +28,20 @@ def main_menu():
   print '3. Exit'
   print 30 * '-'
  
-  #Get user input for main menu choice
-  main_menu_choice = raw_input('Enter a selection [1-3]: ')
+  try:
+    #Get user input for main menu choice
+    main_menu_choice = raw_input('Enter a selection [1-3]: ')
   
-  #Convert choice to a string and error out on  non-integer values
-  try:
-    main_menu_choice = int(main_menu_choice)
-  except:
-   print'That wasn\'t an integer! Please choose 1, 2, or 3.'
-   raw_input()
-   os.system('clear')
-   main_menu()
-
-  #Do stuff based on the choice
-  try:
+    #Convert choice to a string and error out on  non-integer values
+    try:
+      main_menu_choice = int(main_menu_choice)
+    except:
+     print'That wasn\'t an integer! Please choose 1, 2, or 3.'
+     raw_input()
+     os.system('clear')
+     main_menu()
+    
+    #Do stuff based on the choice
     if main_menu_choice == 1:
       file_text() 
     elif main_menu_choice == 2:
@@ -50,55 +50,57 @@ def main_menu():
       print '\nExiting...\n'
       sys.exit()
     else:
-      print '\nNo idea what that was, try something else.'
+      raw_input('\nWhile that was indeed an integer, it wasn\'t 1, 2 or 3. Please try one of those.\n')
       main_menu()
-  except:
-    # We'll call raw_input here so we can pause long enough for the user to read the error message
-    # It doesn't matter what they enter, but it will wait until something is entered before continuing
-    raw_input('Something caused an exception in the main menu. Press any key to exit.')
-    sys.exit(1)
   
-def writing_stats(content):
-  os.system('clear')
-  # Convert content into a list for ease of use.
-  content_list = content.split()
-  # Get a length based on the list
-  wordCount = len(content_list)
-  # Get the top ten words by frequency using the Python 2.7 Counter library
-  topWords = Counter(content_list)
-  topTen = topWords.most_common(10)
-  # Print out our stats
-  print 'Here\'s a few stats on the supplied text: '
-  print '\nNumber of words: %s' % (wordCount)
-  print '\nTop ten most commonly used words and how often they appear in the text: '
-  rank = 1
-  for item in topTen:
-     # Printing nicely formatted tuples without the parens is painful
-     # So we'll use str.format() here
-     print '#{0}: \'{1}\' - occurs {2} times'.format(rank, *item)
-     rank += 1
-
-  # Let's see if they want to find how frequently a specific word shows up
-  specificWordCheckDesired = raw_input('\nWould you like to check how frequently a specific word appears in the text? (y/n)  ')
-  try:
-    if specificWordCheckDesired == 'y':
-      word_to_check = raw_input('\nWhich word would you like to get a frequency count for?\n')
-      # Using that handy counter library 
-      word_count = content_list.count(word_to_check)
-      
-      print '\nThe word \'%s\' occurs %d times in the supplied text.' % (word_to_check, word_count)
-      raw_input('\nPress any key to return to the main menu\n')
-      main_menu()
-    elif specificWordCheckDesired == 'n':
-      raw_input('\nPress any key to exit to the main menu.\n')
-      main_menu() 
-    else:
-      raw_input('That was something other than \'y\' or \'n\', reloading the current menu.')
-      writing_stats(content)
   except:
-   print 'Handled exception - writing stats error:'
-   raw_input('That exception is straight up handled. Press any key to exit to the main menu.')
-   main_menu()
+    raw_input('Handled exception in the main menu, exiting.')
+    sys.exit(1)
+      
+def writing_stats(content):
+  try:  
+    os.system('clear')
+    # Convert content into a list for ease of use.
+    content_list = content.split()
+    # Get a length based on the list
+    wordCount = len(content_list)
+    # Get the top ten words by frequency using the Python 2.7 Counter library
+    topWords = Counter(content_list)
+    topTen = topWords.most_common(10)
+    # Print out our stats
+    print 'Here\'s a few stats on the supplied text: '
+    print '\nNumber of words: %s' % (wordCount)
+    print '\nTop ten most commonly used words and how often they appear in the text: '
+    rank = 1
+    for item in topTen:
+       # Printing nicely formatted tuples without the parens is painful
+       # So we'll use str.format() here
+       print '#{0}: \'{1}\' - occurs {2} times'.format(rank, *item)
+       rank += 1
+
+    # Let's see if they want to find how frequently a specific word shows up
+    specificWordCheckDesired = raw_input('\nWould you like to check how frequently a specific word appears in the text? (y/n)  ')
+    try:
+      if specificWordCheckDesired == 'y':
+        word_to_check = raw_input('\nWhich word would you like to get a frequency count for?\n')
+        # Using that handy counter library 
+        word_count = content_list.count(word_to_check)
+        
+        print '\nThe word \'%s\' occurs %d times in the supplied text.' % (word_to_check, word_count)
+        raw_input('\nPress any key to return to the main menu\n')
+        main_menu()
+      elif specificWordCheckDesired == 'n':
+        raw_input('\nPress any key to exit to the main menu.\n')
+        main_menu() 
+      else:
+        raw_input('That was something other than \'y\' or \'n\', reloading the current menu.')
+        writing_stats(content)
+    except:
+     raw_input('Handled exception in writing stats > specific word check. Press any key to exit to the main menu.')
+     main_menu()
+  except:
+    raw_input('Handled exception in writing stats. Press any key to exit to the main menu.')
+    main_menu()
 
 def pasted_text():
   os.system('clear')
@@ -108,7 +110,7 @@ def pasted_text():
     os.system('clear')
     writing_stats(pasted_text_content)
   except:
-    print 'That didn\'t go as planned, exiting to the main menu.'
+    raw_input('Handled exception in pasted text. Press any key to exit to the main menu.')
     main_menu()  
 
 def file_text():
@@ -134,8 +136,7 @@ def file_text():
         os.system('clear')
         main_menu()
   except:
-    print 'Handled exception - file import:'
-    raw_input('\nThe file reading didn\'t go as planned, exiting to the main menu.\n')
+    raw_input('Handled exception - file import: The file reading didn\'t go as planned, press any key to exit to the main menu.\n')
     main_menu()
   
 # Let's give them a menu
