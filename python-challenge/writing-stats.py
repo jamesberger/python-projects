877d150a59ebb3296ebc3fa19044729691afcdc1
James <james@james.com>
2016-02-01 19:19:50 +0000

    Trying to get proper error handling set up

--- python-challenge/writing-stats.py
+++ python-challenge/writing-stats.py
@@ -41,22 +41,23 @@ def main_menu():
    main_menu()
 
   #Do stuff based on the choice
-  if main_menu_choice == 1:
-    file_text() 
-  elif main_menu_choice == 2:
-    pasted_text()  
-  elif main_menu_choice == 3:
-    os.system('clear')
-    print ('\n"EOL" -Master Control Program\n')
-    sys.exit()
-  else:
+  try:
+    if main_menu_choice == 1:
+      file_text() 
+    elif main_menu_choice == 2:
+      pasted_text()  
+    elif main_menu_choice == 3:
+      print '\nExiting...\n'
+      sys.exit()
+    else:
+      print '\nNo idea what that was, try something else.'
+      main_menu()
+  except:
     # We'll call raw_input here so we can pause long enough for the user to read the error message
     # It doesn't matter what they enter, but it will wait until something is entered before continuing
-    raw_input('That was something other than 1, 2 or 3. Please select one of those options.')
-    # Now let's clear the screen and bring up the menu again
-    os.system('clear')
-    main_menu()
-
+    raw_input('Something caused an exception in the main menu. Press any key to exit.')
+    sys.exit(1)
+  
 def writing_stats(content):
   os.system('clear')
   # Convert content into a list for ease of use.
@@ -86,52 +87,60 @@ def writing_stats(content):
       word_count = content_list.count(word_to_check)
       
       print '\nThe word \'%s\' occurs %d times in the supplied text.' % (word_to_check, word_count)
-      raw_input('\nPress any key to return to the main menu')
+      raw_input('\nPress any key to return to the main menu\n')
       main_menu()
     elif specificWordCheckDesired == 'n':
-      main_menu()  
+      raw_input('\nPress any key to exit to the main menu.\n')
+      main_menu() 
+    else:
+      raw_input('That was something other than \'y\' or \'n\', reloading the current menu.')
+      writing_stats(content)
   except:
-   print('Writing stats error:  That wasn\'t a \'y\' or a \'n\'. Please choose \'y\' or a \'n\'.')
-   #writing_stats(content) 
+   print 'Handled exception - writing stats error:'
+   raw_input('That exception is straight up handled. Press any key to exit to the main menu.')
    main_menu()
 
 def pasted_text():
   os.system('clear')
-  # Getting user input (note this is Python 2.x specific)
-  pasted_text_content = raw_input('The text god demands text! Enter it below: \n')
-  os.system('clear')
-  writing_stats(pasted_text_content)
+  try:
+    # Getting user input (note this is Python 2.x specific)
+    pasted_text_content = raw_input('The text god demands text! Enter it below: \n')
+    os.system('clear')
+    writing_stats(pasted_text_content)
+  except:
+    print 'That didn\'t go as planned, exiting to the main menu.'
+    main_menu()  
 
 def file_text():
   os.system('clear')
-  file_path = raw_input('\nEnter the path for your filename, like /home/bob/my-story.txt: ')
-  if os.path.isfile(file_path):
-    print('\nFile found.')
-    user_file = open(file_path, 'r')
-    file_contents = user_file.read();
-    print('\nReading...')
-    raw_input('\nPress any key to proess the file.')
-    user_file.close()
-    writing_stats(file_contents)
-  else:
-    file_text_menu_choice = raw_input('\nFile not found. \n\nPress \'r\' to re-try or \'e\' to exit to the main menu.\n')
-    if file_text_menu_choice == 'r':
-      file_text()
-    elif file_text_menu_choice == 'e':
-      main_menu()
+  try:
+    file_path = raw_input('\nEnter the path for your filename, like /home/bob/my-story.txt: ')
+    if os.path.isfile(file_path):
+      print('\nFile found.')
+      user_file = open(file_path, 'r')
+      file_contents = user_file.read();
+      print('\nReading...')
+      raw_input('\nPress any key to continue.\n')
+      user_file.close()
+      writing_stats(file_contents)
     else:
-      raw_input('That was something other than \'r\' or \'e\'. Exiting to main menu.')
-      os.system('clear')
-      main_menu()
-  pass
-
+      file_text_menu_choice = raw_input('\nFile not found. \n\nPress \'r\' to re-try or \'e\' to exit to the main menu.\n')
+      if file_text_menu_choice == 'r':
+        file_text()
+      elif file_text_menu_choice == 'e':
+        main_menu()
+      else:
+        raw_input('That was something other than \'r\' or \'e\'. Exiting to main menu.\n')
+        os.system('clear')
+        main_menu()
+  except:
+    print 'Handled exception - file import:'
+    raw_input('\nThe file reading didn\'t go as planned, exiting to the main menu.\n')
+    main_menu()
+  
 # Let's give them a menu
 main_menu()
 
-# If we exit a sub menu, we should bring up the main menu again
-main_menu()
-
 # Otherwise? Let's exit then
-print '\n"EOL" -Master Control Program'
 sys.exit()
 
