f9cca772d6eb992a2e1a1c15e6ee67bd3e76b0d2
James <james@james.com>
2016-02-16 20:36:34 +0000

    So yeah, this is how FizzBuzz works in Python

--- fizzbuzz/fizzbuzz.py
+++ fizzbuzz/fizzbuzz.py
@@ -1,9 +1,19 @@
 #!/usr/bin/env python27
 
 '''
- Sexy one liner:
- for i in range(1,101): print "FizzBuzz"[i*i%3*4:8--i**4%5] or i
+Notes on FizzBuzz as implemented in Python
+James Berger
+February 16th, 2016
+
+Sexy one liner found via Google:
+for i in range(1,101): print "FizzBuzz"[i*i%3*4:8--i**4%5] or i
+
+Terrible Python, but the fact that it's on one line is strangely appealing.
 '''
+
+# A more comprehensible solution to FizzBuzz in Python
+# AKA "Do you know how to divide a number in Python?"
+# While we're at it, why did people go with 'modulus' instead of good 'ol 'division' and 'remainder'?
 
 for number in range(1,101):
   result = ''
