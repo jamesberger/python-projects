a9eda7d4a63e09cfdddf49eb13d364e6bf9b6ede
James Berger <james.m.berger@gmail.com>
2022-10-24 12:34:27 -0500

    More progress

--- backup-photos-to-s3/backup-photos-to-s3.py
+++ backup-photos-to-s3/backup-photos-to-s3.py
@@ -2,6 +2,7 @@
 
 import boto3
 import botocore.exceptions
+import exif 
 import logging
 import os
 import sys
@@ -33,33 +34,101 @@ import sys
 # target_directory = '/home/myuser/photos'
 # target_s3_bucket = 's3://mybucketname/myphotos'
 # target_credentials_file = '/home/myuser/.aws/credentials'
-target_directory = '/home/james'
-target_s3_bucket = ''
-target_credentials_file = '/home/blah/credentials'
+target_directory = '/home/james/exif-photo-test'
+target_s3_bucket = 's3://'
+target_credentials_file = '/home/james/.aws/credentials'
+images_list = []
 
 # Set up logger
 #logging.basicConfig(level=logging.WARN)
 #logger = logging.getLogger()
 
-# Check to see if the target locations exist:
-try:
-    if os.path.exists(target_directory) == False:
-        sys.exit('BPS3: Could not find target directory ' + target_directory + ', exiting.')
-except Exception as e:
-    sys.exit(e)
+def initial_checks():
+
+    # Check to see if the target locations exist:
+    try:
+        if os.path.exists(target_directory) == False:
+            sys.exit('BPS3: Could not find target directory ' + target_directory + ', exiting.')
+    except Exception as e:
+        sys.exit(e)
 
 
-# Check to see if the AWS credentials are valid
-try: 
-    client = boto3.client('sts')
-    client.get_caller_identity()
-except botocore.exceptions.NoCredentialsError:
-    sys.exit('BPS3: Could not validate credentials via boto3.client.get_caller_identity(), exiting.')
-else:
-    aws_credentials_valid = True
+    # Check to see if the AWS credentials are valid
+    try: 
+        client = boto3.client('sts')
+        client.get_caller_identity()
+    except botocore.exceptions.NoCredentialsError:
+        sys.exit('BPS3: Could not validate credentials via boto3.client.get_caller_identity(), exiting.')
+    else:
+        aws_credentials_valid = True
 
-# Check to see if S3 bucket exists if our AWS credential check succeeds
-if aws_credentials_valid == True:
-    response = client.head_bucket(
-        Bucket = target_s3_bucket,
-    )
+    # Check to see if S3 bucket exists if our AWS credential check succeeds
+    if aws_credentials_valid == True:
+        response = client.head_bucket(
+            Bucket = target_s3_bucket,
+        )
+
+def build_file_list():
+    for (dirpath, dirnames, filenames) in os.walk(target_directory):
+        images_list.extend(filenames)
+        # Note: We need to grab the path to the file name as well,
+        # in order to handle subdirectories in the target directory
+
+    return images_list
+
+def get_photo_exif_data(images_list):
+
+
+    for image in images_list:
+
+        image_with_path = target_directory + '/' + image
+
+        with open(image_with_path, 'rb') as image_file:
+            target_image = exif.Image(image_file)
+
+        if target_image.has_exif:
+            photo_taken_date = target_image.datetime_digitized
+
+            print(photo_taken_date)
+        
+
+
+
+def sort_photos(target_directory):
+
+    print('check')
+
+    return images_list
+
+
+def upload_photos(file_name, bucket, object_name=None):
+    """Upload a file to an S3 bucket
+
+    :param file_name: File to upload
+    :param bucket: Bucket to upload to
+    :param object_name: S3 object name. If not specified then file_name is used
+    :return: True if file was uploaded, else False
+    """
+
+    # If S3 object_name was not specified, use file_name
+    if object_name is None:
+        object_name = os.path.basename(file_name)
+
+    # Upload the file
+    s3_client = boto3.client('s3')
+    try:
+        response = s3_client.upload_file(file_name, bucket, object_name)
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return True
+
+if __name__ == '__main__':
+
+   # initial_checks()
+
+    build_file_list()
+    for image in images_list:
+        print(image)
+
+    get_photo_exif_data(images_list)
