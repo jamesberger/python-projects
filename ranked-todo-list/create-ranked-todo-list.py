46472c32d5c87423419604eb1cb07a7da2c34856
James Berger <james.m.berger@gmail.com>
2024-04-09 12:28:49 -0500

    Initial draft for the ranked todo list

---
+++ ranked-todo-list/create-ranked-todo-list.py
@@ -0,0 +1,9 @@
+"""
+Creating a ranked todo list:
+
+Step 1: Enter list of todo items as a dictionary
+Step 2: Assign an initial rank of 0 for the value for each item in the dictionary.
+Step 3: Perform a comparison of each todo item against every other item in the dictionary.
+Step 4: For each item that is compared, increment the rank value of that item by 1 if it's more important than the current item. Otherwise, increase the rank of the current item.
+Step 5: Print a ranked list of the todo items. Also maybe provide an option to export the ranked items into a CSV?
+"""
