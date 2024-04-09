db553fd85ae786915f861bc40ce4cd4038c41e98
James Berger <james.m.berger@gmail.com>
2024-04-09 13:43:34 -0500

    Initial function to add items to the dictionary

--- ranked-todo-list/create-ranked-todo-list.py
+++ ranked-todo-list/create-ranked-todo-list.py
@@ -7,3 +7,15 @@ Step 3: Perform a comparison of each todo item against every other item in the d
 Step 4: For each item that is compared, increment the rank value of that item by 1 if it's more important than the current item. Otherwise, increase the rank of the current item.
 Step 5: Print a ranked list of the todo items. Also maybe provide an option to export the ranked items into a CSV?
 """
+
+def add_item(item):
+    # Check to see if todo_items dictionary exists
+    if 'todo_items' not in globals():
+        globals()['todo_items'] = {}
+
+    # Assign the item to our dictionary with an initial rank of zero
+    todo_items[item] = 0
+
+
+
+    
